697b06766f67e40fe813355920e62e30ad9d1164
Santiago <projectmelody11@gmail.com>
2022-03-30 12:41:13 -0500

    Projecto React y js

---
+++ freecamp/src/componentes/testimonio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function testimonio(){
+    return (
+        <div className='contenedor-testimonio'>
+            <img
+            className='imagen-testimonio'
+            src='' />
+
+        </div>
+    );
+}
